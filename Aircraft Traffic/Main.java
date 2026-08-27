76170646076c690414f71b55607808a67f239e24
uyennguyen-06 <uyen.nguyen3@students.mq.edu.au>
2026-08-27 19:50:48 +1000

    update

--- Aircraft Traffic/Main.java
+++ Aircraft Traffic/Main.java
@@ -1,6 +1,7 @@
-class Main{
+public class Main{
     public static void main (String[] args){
-        Plane tester = new Plane();
-        tester.setPlane("89797", "Billy", "SuperBus", 50.00, 20, "Grounded");
+        System.out.println("Program started!");
+        /*Plane tester = new Plane();
+        tester.setPlane("89797", "Billy", "SuperBus", 50.00, 20, "Grounded");*/
     }
 }
