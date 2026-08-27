ae34f66512e6d816ae5a33ebbe1154388675f3e8
uyennguyen-06 <uyen.nguyen3@students.mq.edu.au>
2026-08-27 18:37:55 +1000

    setters and getters for AirwayGate

--- Aircraft Traffic/AirwayGate.java
+++ Aircraft Traffic/AirwayGate.java
@@ -1,35 +1,46 @@
 public class AirwayGate {
-    private String gateID;
+    private final String gateID;
     private boolean status; // true if the gate is open, false if closed
     private Plane currentPlane; // the plane currently at the gate
 
-    public AirwayGate(String gateID, boolean status, Plane plane) {
+    public AirwayGate(String gateID, boolean status, Plane currentPlane) {
         this.gateID = gateID;
         this.status = status;
-        this.placurrentPlanene = plcurrentPlaneane;
+        this.currentPlane = currentPlane;
     }
 
+    //setters
+    public boolean parkPlane (Plane plane){
+        if (status && currentPlane == null){
+            currentPlane = plane;
+            return true;
+        }
+        return false;
+    }
+
+    public Plane removePlane(){
+        Plane departingPlane = currentPlane;
+        currentPlane = null;
+        return departingPlane;
+    }
+
+    //getters
     public String getGateID() {
         return gateID;
     }
 
     public boolean getStatus() {
-        if (status) {
-            System.out.println("The gate is currently open.");
-        } else {
-            System.out.println("The gate is currently closed.");
-        }
         return status;
     }
 
     public Plane getCurrentPlane() {
-        return plcurrentPlaneane;
+        return currentPlane;
     }
 
     public void displayInfo() {
         System.out.println("Gate ID: " + gateID);
         System.out.println("Status: " + (status ? "Open" : "Closed"));
-        if (plane != null) {
+        if (currentPlane != null) {
             System.out.println("Plane at gate: " + currentPlane.getPlaneID());
         } else {
             System.out.println("No plane currently at the gate.");
